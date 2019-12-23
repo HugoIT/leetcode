04287331a41eea78275dfe583f418ce633c29ca3
xiongyougui <yougui.xiong@zhangmen.com>
2019-12-23 14:48:12 +0800

    init

---
+++ src/main/java/com/programmer/talk/Hello.java
@@ -0,0 +1,9 @@
+package com.programmer.talk;
+
+/**
+ * @Author: Hsiung
+ * @Date: 2019/12/23 14:46
+ * @Description TODO
+ */
+public class Hello {
+}
---
+++ src/test/java/TestHello.java
@@ -0,0 +1,7 @@
+/**
+ * @Author: Hsiung
+ * @Date: 2019/12/23 14:47
+ * @Description TODO
+ */
+public class TestHello {
+}
