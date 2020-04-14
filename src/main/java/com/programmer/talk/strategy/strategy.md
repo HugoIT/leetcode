0cad956a8f99cf2dcfbdd438735235aacc40e35b
yougui.xiong <yougui.xiong@zhangmen.com>
2020-04-15 00:45:51 +0800

    动态规划

--- src/main/java/com/programmer/talk/strategy/strategy.md
+++ src/main/java/com/programmer/talk/strategy/strategy.md
@@ -1,1 +1,4 @@
 # leetcode 策略【套路】
+
+#### 1. 动态规划（Dynamic programming）
+- [双蛋问题](https://leetcode-cn.com/problems/super-egg-drop/)
---
+++ src/main/java/com/programmer/talk/strategy/super_egg_drop/Solution1.java
@@ -0,0 +1,68 @@
+package com.programmer.talk.strategy.super_egg_drop;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * #887 https://leetcode-cn.com/problems/super-egg-drop/
+ * <p>
+ * 1.假设只有1个鸡蛋
+ * 2.假设有无限个鸡蛋
+ * 3.假设有2个鸡蛋
+ *
+ * @author Hugo
+ */
+public class Solution1 {
+	/**
+	 * 动态规划
+	 *
+	 * @param K 鸡蛋个数
+	 * @param N 楼层总数
+	 * @return 最少移动次数
+	 */
+	public int superEggDrop(int K, int N) {
+		return dp(K, N);
+	}
+
+	Map<Integer, Integer> memo = new HashMap();
+
+	public int dp(int K, int N) {
+		if (!memo.containsKey(N * 100 + K)) {
+			int ans;
+			// 0个鸡蛋
+			if (N == 0) {
+				ans = 0;
+			} else if (K == 1) {
+				// 1个鸡蛋
+				ans = N;
+			} else {
+				int lo = 1, hi = N;
+				/**
+				 * 二分法
+				 */
+				while (lo + 1 < hi) {
+					int x = (lo + hi) / 2;
+					int t1 = dp(K - 1, x - 1);
+					int t2 = dp(K, N - x);
+
+					if (t1 < t2) {
+						lo = x;
+					} else if (t1 > t2) {
+						hi = x;
+					} else {
+						lo = hi = x;
+					}
+				}
+
+				ans = 1 + Math.min(Math.max(dp(K - 1, lo - 1), dp(K, N - lo)),
+						Math.max(dp(K - 1, hi - 1), dp(K, N - hi)));
+			}
+
+			memo.put(N * 100 + K, ans);
+		}
+
+		return memo.get(N * 100 + K);
+	}
+
+}
+
